2144a1b4978290556986a02a695e1072b441de5c
Ricardo_Flores <ricardo.flores1701@alumnos.ubiobio.cl>
2022-11-12 01:01:21 -0300

    implementación model-controller-routes 'casa'

--- Backend/index.js
+++ Backend/index.js
@@ -4,20 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const productRoutes = require('./routes/productRoutes');
-const categoryRoutes = require('./routes/categoryRoutes')
 const casaRoutes = require('./routes/casaRoutes')
-const propietarioRoutes = require('./routes/propietarioRoutes')
-const gastoscomunesRoutes = require('./routes/gastoscomunesRoutes')
+
 
 app.use(cors())
 app.use(express.json());
 app.options('*', cors());
-app.use('/api', productRoutes);
-app.use('/api', categoryRoutes);
+
 app.use('/api', casaRoutes);
-app.use('/api', propietarioRoutes);
-app.use('/api', gastoscomunesRoutes);
+
 
 
 
--- Backend/routes/propietarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const api = express.Router();
-const propietarioController = require('../controllers/propietarioController');
-
-api.post('/propietarioModel', propietarioController.createPropietario);
-api.get('/propietarioModels', propietarioController.getPropietarios);
-
-
-module.exports = api;
