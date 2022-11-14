4c3802166ad9261a9af28f4c82714a900f15ced7
moises <m.caceres2565@gmail.com>
2022-11-14 02:57:09 -0300

    resolucion de conflictos merge

--- Backend/routes/casaRoutes.js
+++ Backend/routes/casaRoutes.js
@@ -4,12 +4,8 @@ const casaController = require('../controllers/casaController');
 
 api.post('/casaModel', casaController.createCasa);
 api.get('/casaModels', casaController.getCasas);
-<<<<<<< HEAD
-api.delete('/casaModels', casaController.getCasas);
-=======
 api.get('/casaModel/search/numero/:numero_casa', casaController.getSpecificCasaWithNumero);
 
 
->>>>>>> desarollo_moises
 
 module.exports = api;
